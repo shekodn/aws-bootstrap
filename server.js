cf37aaf08e1b944668cf6c903cb5e5ca561fa742
shekodn <sdiazn19@gmail.com>
2020-01-23 13:15:18 -0600

    Change greeting

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { hostname } = require('os');
 const http = require('http');
 
-const message = 'hello world';
+const message = 'hello cloud \n';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
