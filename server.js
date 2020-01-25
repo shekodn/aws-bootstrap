9fbb10af023fd331c608f60eb6c6040814ada28d
shekodn <sdiazn19@gmail.com>
2020-01-25 14:44:01 -0600

    Add stack name to server output

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = `Hello World from ${hostname()}\n`;
+const STACK_NAME = process.env.STACK_NAME || "Unknown Stack";
+const message = `Hello World from ${hostname()} in ${STACK_NAME}\n`;
 const port = 8080;
 
 const server = http.createServer((req, res) => {
@@ -10,5 +11,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`server running at http://${hostname()}:${port}/`);
+  console.log(`Server running at http://${hostname()}:${port}/`);
 });
